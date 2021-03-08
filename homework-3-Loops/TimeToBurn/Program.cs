38831fbca504655fe0a3b2e8df12c8eb3f08f3ff
Dean Derebeew <deanderebeew@ou.edu>
2021-03-07 19:31:26 -0600

    Created a solution for homework 3 and finished the TimeToBurn project

---
+++ homework-3-Loops/TimeToBurn/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TimeToBurn
+{
+    class Program
+    {
+        const double Calories_Burned = 3.9;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your calorie goal that you would you like to burn?");
+            string answer = Console.ReadLine();
+
+            double goal;
+            if (double.TryParse(answer, out goal) == false)
+            {
+                Console.WriteLine($"{answer} was an invalid goal. Goodbye!");
+                Environment.Exit(-1);
+
+            }
+            double calories_burned_sofar = 0;
+            int minutes = 0;
+
+            if (goal == 0)
+            {
+                Console.WriteLine("Come on, you can do better than that!");
+            }
+
+            else
+            {
+
+                while (calories_burned_sofar < goal)
+                {
+                    minutes++;
+                    calories_burned_sofar = calories_burned_sofar + Calories_Burned;
+                    Console.WriteLine($"After {minutes.ToString("N0")} minutes, you've burned {calories_burned_sofar.ToString("N2")}");
+
+                }
+           
+            }
+        }
+
+    }
+}
