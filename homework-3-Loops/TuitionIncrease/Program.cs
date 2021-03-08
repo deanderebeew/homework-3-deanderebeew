9a7b5c4fe78261d4711a7828d6926c5bb0c8efbd
Dean Derebeew <deanderebeew@ou.edu>
2021-03-07 20:26:39 -0600

    Finished Tuition increase completing homework 3

--- homework-3-Loops/TuitionIncrease/Program.cs
+++ homework-3-Loops/TuitionIncrease/Program.cs
@@ -6,7 +6,18 @@ namespace TuitionIncrease
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double tuition = 12000;
+            const double Tuition_Increase = 0.05;
+
+            for (int i = 1; i <= 7; i++)
+            {
+                //tuition = tuition * (1 + Tuition_Increase);
+                tuition *= (1 + Tuition_Increase);
+                Console.WriteLine($"The tuition after year {i} will be {tuition.ToString("C0")}.");
+            }
+         
+
+
         }
     }
 }
